f26a0fd197b482e0b87229289455175931f17d98
TheFlyingPizzaHuose <85021898+TheFlyingPizzaHuose@users.noreply.github.com>
2021-09-28 16:12:43 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -2,19 +2,32 @@ console.log('server.js started');
 
 //Referencing dependecies
 const express = require('express');
-const body_parser = require("body-parser");
 const path = require('path');
-const { dirname } = require('path');
 const app = express();
+const fs = require('fs')
+const date_ob = new Date();
 console.log('dependencies import success');
 
 app.use(express.static(__dirname));
 
 express.static.mime.define({'application/javascript': ['js']});
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/index.html'));
-  console.log('Website Visited')
+function readWriteSync() {
+  var data = fs.readFileSync('log.txt', 'utf-8');
+  data+=('\nMM/DD/HH/mm: '+date_ob.getMonth()+'/'+date_ob.getDate()+'/'+date_ob.getHours()+'/'+date_ob.getMinutes());
+  fs.writeFileSync('log.txt', data, 'utf-8');
+}
+app.get('/', (req,res)=>{
+  res.redirect('/main')
+})
+app.get('/main', (req, res) => {
+  readWriteSync();
+  res.sendFile(path.join(__dirname + '/main.html'));
 });
+
+
+app.get('/screenOnly', (req, res)=>{
+  res.sendFile(path.join(__dirname + '/fullscreen.html'));
+})
 
 app.listen(5000);
--- shading.js
+++ shading.js
@@ -4,9 +4,9 @@ import {vectorSubtract, vectorScalar, arrayEqual} from './math.js';
 function generateWireframe(coords, width, height){
     var line1 = [];
     if(checkTriangleCull(coords, width, height)){
-        line1 = generateLine(coords[0], coords[1]);
-        var line2 = generateLine(coords[0], coords[2]);
-        var line3 = generateLine(coords[1], coords[2]);
+        line1 = generateLine(coords[0], coords[1], width);
+        var line2 = generateLine(coords[0], coords[2], width);
+        var line3 = generateLine(coords[1], coords[2], width);
         line2.forEach(value=>{
             line1.push(value);
         })
@@ -32,7 +32,7 @@ function checkTriangleCull(coords, cull, cull2){
 }
 
 //Interpolates between two points and returns the values
-function generateLine(point1, point2, cull=false, cull2=false, shadingMode=false){
+function generateLine(point1, point2, cull=false, shadingMode=false){
     var result = [];
 
     //Gets slopes
@@ -49,9 +49,8 @@ function generateLine(point1, point2, cull=false, cull2=false, shadingMode=false
     }
     else if(shadingMode){
         var increment = Math.sign(difference[0]);
-        for(var i = 0; i!=difference[0]; i+=increment){
-            result.push([parseInt(currentX), parseInt(currentY)]);
-            currentX+=increment;
+        for(var i = point1[0]+=increment; i!=point2[0]; i+=increment){
+            result.push([parseInt(i), currentY]);
             //Camera Culling
             if(cull != false){
                 if(currentX < -cull && increment == -1){
@@ -61,16 +60,13 @@ function generateLine(point1, point2, cull=false, cull2=false, shadingMode=false
                 }
             }
         }
-        result.push([parseInt(currentX), parseInt(currentY)]);
     }else{
         var increment = Math.sign(difference[1]);
         slope = slope*increment;
-        for(var i = 0; i!=difference[1]; i+=increment){
-            result.push([parseInt(currentX), parseInt(currentY)]);
+        for(var i=point1[1]; i!=point2[1]+increment; i+=increment){
+            result.push([parseInt(currentX), parseInt(i)]);
             currentX+=slope;
-            currentY+=increment;
         }
-        result.push([parseInt(currentX), parseInt(currentY)]);
     }
     return result;
 }
@@ -78,11 +74,10 @@ function generateLine(point1, point2, cull=false, cull2=false, shadingMode=false
 //Generates a depth map based on 2d coordinates and 3d coordinates
 function generateZMap(coords, shade, color, width, height){
     var shaded = vectorScalar(color, shade);
-    shaded = [parseInt(shaded[0]), parseInt(shaded[1]), parseInt(shaded[2])];
+    shaded = [parseInt(shaded[0]), parseInt(shaded[1]), parseInt(shaded[2]), 255];
 
     //Generates wirframe
-    var lines = generateWireframe(coords, width, height);
-    var lines2 = [];
+    var lines = generateWireframe(coords, width, height, shaded);
 
     var pairs = [];
     //Find pairs
@@ -100,18 +95,17 @@ function generateZMap(coords, shade, color, width, height){
         //Rearrange data (cuz my brain can't be bothered to write it in 1 line)
         var arg1 = [value[0][0], value[0][1]];
         var arg2 = [value[1][0], value[1][1]];
-        generateLine(arg1, arg2, width, height, true).forEach(value2=>{
-            lines2.push(value2);
+        generateLine(arg1, arg2, width, true).forEach(value2=>{
+            lines.push(value2);
         })
     })
-
     
     //Get distances
-    lines2.forEach((value,index)=>{
-        lines2[index][5] = shaded;
+    lines.forEach((value,index)=>{
+        lines[index][2] = shaded;
     })
 
-    return lines2;
+    return lines;
 }
 
 export {generateZMap, generateWireframe};
