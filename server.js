4569bd543beb448f2377b333cc6f044547f4a73d
TheFlyingPizzaHuose <85021898+TheFlyingPizzaHuose@users.noreply.github.com>
2021-10-07 10:43:18 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const date_ob = new Date();
 console.log('dependencies import success');
 
 app.use(express.static(__dirname));
+app.disable('etag');
 
 express.static.mime.define({'application/javascript': ['js']});
 
@@ -17,15 +18,17 @@ function readWriteSync() {
   data+=('\nMM/DD/HH/mm: '+date_ob.getMonth()+'/'+date_ob.getDate()+'/'+date_ob.getHours()+'/'+date_ob.getMinutes());
   fs.writeFileSync('log.txt', data, 'utf-8');
 }
+
 app.get('/', (req,res)=>{
   res.redirect('/main')
 })
+
 app.get('/main', (req, res) => {
   readWriteSync();
+  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
   res.sendFile(path.join(__dirname + '/main.html'));
 });
-
-
 app.get('/screenOnly', (req, res)=>{
   res.sendFile(path.join(__dirname + '/fullscreen.html'));
 })
